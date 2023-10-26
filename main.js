643a096f7caa2d40262fff03ef5bea6b49efa446
DinFloat <134521952+DinFloat@users.noreply.github.com>
2023-10-26 12:21:16 -0300

    shoryuken

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomShoryuken(){
+    document.querySelector('#som_tecla_shoryuken').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_shoryuken').onclick = tocaSomShoryuken;
 
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
