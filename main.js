3561647bb7f662c2fdc7acb47c656c8fd6bc469f
DinFloat <134521952+DinFloat@users.noreply.github.com>
2023-10-27 09:10:08 -0300

    NOVA CONEXÃO

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function tocaSomShoryuken(){
-    document.querySelector('#som_tecla_shoryuken').play();
-}
-document.querySelector('.tecla_shoryuken').onclick = tocaSomShoryuken;
-
-function tocaSomKid(){
-    document.querySelector('#som_tecla_kid').play();
-}
-document.querySelector('.tecla_kid').onclick = tocaSomKid;
-
-function tocaSomBemVindo(){
-    document.querySelector('#som_tecla_bem_vindo').play();
-}
-document.querySelector('.tecla_bem_vindo').onclick = tocaSomBemVindo;
-
-//divisão
 
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
---
+++ newMain.js
@@ -0,0 +1,14 @@
+function tocaSomShoryuken(){
+    document.querySelector('#som_tecla_shoryuken').play();
+}
+document.querySelector('.tecla_shoryuken').onclick = tocaSomShoryuken;
+
+function tocaSomKid(){
+    document.querySelector('#som_tecla_kid').play();
+}
+document.querySelector('.tecla_kid').onclick = tocaSomKid;
+
+function tocaSomBemVindo(){
+    document.querySelector('#som_tecla_bem_vindo').play();
+}
+document.querySelector('.tecla_bem_vindo').onclick = tocaSomBem;
