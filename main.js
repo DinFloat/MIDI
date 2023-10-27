9cbb19e0b5aa043331a9e7fa7e4a54bb0d9a0554
DinFloat <134521952+DinFloat@users.noreply.github.com>
2023-10-27 12:21:44 -0300

    ..

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function tocaSomShoryuken(){
+    document.querySelector('#som_tecla_shoryuken').play();
+}
+document.querySelector('.tecla_shoryuken').onclick = tocaSomShoryuken;
+
+function tocaSomKid(){
+    document.querySelector('#som_tecla_kid').play();
+}
+document.querySelector('.tecla_kid').onclick = tocaSomKid;
+
+function tocaSomBemVindo(){
+    document.querySelector('#som_tecla_bem_vindo').play();
+}
+document.querySelector('.tecla_bem_vindo').onclick = tocaSomBemVindo;
+
+//espaço
 
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
